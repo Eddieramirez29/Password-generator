02433419c978d007bfaf4bb56a3447c4c073a687
Eduardo <tkd13000@gmail.com>
2024-06-05 20:22:41 -0600

    Check boxes were added, but without order

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const lengthValue = document.getElementById("characterLength");
 slider.addEventListener('input', function()
 {
     const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
-    characterLength.innerHTML = value/5;
+    characterLength.innerHTML = Math.floor(value/5);//Se asegura de redondearlo hacia abajo, ya que en el 11 desplega un número decimal
     slider.style.background = `linear-gradient(to right, #00ff00 ${value}%, #000000 ${value}%)`;
 });
