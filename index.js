cd77ce604a093840e05ba5ca5e13c97593c1f2ce
Eduardo <tkd13000@gmail.com>
2024-06-05 19:32:15 -0600

    Slide bar set up and length value of the character also was set up

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const slider = document.getElementById('slider');
+const lengthValue = document.getElementById("characterLength");
 
-slider.addEventListener('input', function() {
+slider.addEventListener('input', function()
+{
     const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
+    characterLength.innerHTML = value/5;
     slider.style.background = `linear-gradient(to right, #00ff00 ${value}%, #000000 ${value}%)`;
 });
