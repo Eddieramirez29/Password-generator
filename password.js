2c76b168467228e91cd94411312f3182d179c08d
Eduardo <tkd13000@gmail.com>
2024-06-28 22:57:40 -0600

    password is displayed on my browser

--- password.js
+++ password.js
@@ -1,24 +1,29 @@
-const crypto = require('crypto');
+let newPassword = document.querySelector('.pass');
+let generatePasswordButton = document.getElementById("generatePassword")
 
 // Función para generar una contraseña aleatoria en Node.js
 function generateRandomPassword(length) {
     // Conjunto de caracteres que se pueden usar en la contraseña
-    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~|}{[]:;?><,./-=';
     const charsetLength = charset.length;
     let password = '';
 
-    // Usamos crypto.randomBytes para obtener valores aleatorios seguros
-    const randomBytes = crypto.randomBytes(length);
+    const randomValues = new Uint32Array(length);
+    window.crypto.getRandomValues(randomValues);
 
     for (let i = 0; i < length; i++) {
         // Usamos los valores aleatorios para seleccionar caracteres del charset
-        password += charset[randomBytes[i] % charsetLength];
+        password += charset.charAt(randomValues[i] % charsetLength);
     }
 
     return password;
 }
 
-// Ejemplo de uso: generar una contraseña de 12 caracteres
-const password = generateRandomPassword(12);
-console.log(password);
 
+
+generatePasswordButton.addEventListener('click', () => {
+    // Ejemplo de uso: generar una contraseña de 12 caracteres
+    const password = generateRandomPassword(12);
+    newPassword.textContent  = password;
+    console.log(password);
+});
